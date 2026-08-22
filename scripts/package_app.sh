862ce8329f72a616cca6bcd1b279f0425194dcf8
JoonNyung Heo <jnheo@jnheo.com>
2026-08-23 08:21:51 +0900

    Prepare v1.5.1 release

--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -55,9 +55,9 @@ cat > "${CONTENTS_DIR}/Info.plist" <<EOF
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.5.0</string>
+    <string>1.5.1</string>
     <key>CFBundleVersion</key>
-    <string>6</string>
+    <string>7</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>CFBundleIconFile</key>
